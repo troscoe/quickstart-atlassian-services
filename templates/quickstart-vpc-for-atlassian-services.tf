0f0279a28bdd3450855753e84f32edce587658df
Tristan Roscoe <troscoe@gmail.com>
2020-02-09 13:36:46 -0500

    Update quickstart-vpc-for-atlassian-services.tf

--- templates/quickstart-vpc-for-atlassian-services.tf
+++ templates/quickstart-vpc-for-atlassian-services.tf
@@ -12,7 +12,11 @@ module "vpc" {
   private_subnets = var.PrivateSubnetCIDRs
   public_subnets  = var.PublicSubnetCIDRs
   
+  enable_ipv6 = true
+  
   enable_nat_gateway = true
   single_nat_gateway = false
   one_nat_gateway_per_az = true
+  
+  private_dedicated_network_acl = true
 }
