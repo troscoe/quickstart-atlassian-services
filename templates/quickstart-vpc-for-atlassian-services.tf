bc42d363c23991f20d61bffc92d5c7ebfe243652
Tristan Roscoe <troscoe@gmail.com>
2020-02-09 15:23:59 -0500

    Update quickstart-vpc-for-atlassian-services.tf

--- templates/quickstart-vpc-for-atlassian-services.tf
+++ templates/quickstart-vpc-for-atlassian-services.tf
@@ -20,3 +20,30 @@ module "vpc" {
   
   private_dedicated_network_acl = true
 }
+
+module "bastion" {
+  source = "terraform-aws-modules/bastion/aws"
+  bucket_name = join("-", [var.ExportPrefix, "bastion-bucket", module.vpc.id])
+  region = var.region
+  vpc_id = module.vpc.id
+  create_dns_record = "false"
+  is_lb_private = "false"
+  bastion_host_key_pair = aws_key_pair.generated_key.key_name
+  elb_subnets = var.PublicSubnetCIDRs
+  auto_scaling_group_subnets = var.PublicSubnetCIDRs
+}
+
+resource "aws_s3_bucket" "bastion" {
+  bucket = join("-", [var.ExportPrefix, "bastion-bucket", module.vpc.id])
+  acl    = "private"
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = join("-", [var.ExportPrefix, "bastion-key", module.vpc.id])
+  public_key = "tls_private_key.example.public_key_openssh
+}
