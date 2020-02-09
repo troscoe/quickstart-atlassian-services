f81505b19ffbe9fccbb67c5479ef1a53bbc0e10f
Tristan Roscoe <troscoe@gmail.com>
2020-02-09 11:44:12 -0500

    Update quickstart-vpc-for-atlassian-services.tf

--- templates/quickstart-vpc-for-atlassian-services.tf
+++ templates/quickstart-vpc-for-atlassian-services.tf
@@ -1,13 +1,10 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = var.StackName
   cidr = var.VPCCIDR
 
   azs             = var.AvailabilityZones[var.region]
   private_subnets = var.PrivateSubnetCIDRs
   public_subnets  = var.PrivateSubnetCIDRs
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
 }
